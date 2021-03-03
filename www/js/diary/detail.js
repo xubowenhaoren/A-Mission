859f210fe4ea083c40af7c72c4e15c12e97b0dcd
bowenxu <xubowenhaoren@gmail.com>
2021-03-02 21:05:34 -0800

    remove the speed graph

--- www/js/diary/detail.js
+++ www/js/diary/detail.js
@@ -107,46 +107,46 @@ angular.module('emission.main.diary.detail',['ui-leaflet', 'ng-walkthrough',
     });
   }
 
-  console.log("trip.start_place = " + JSON.stringify($scope.trip.start_place));
-
-  var data  = [];
-  var start_ts = $scope.trip.properties.start_ts;
-  var totalTime = 0;
-  for (var s in $scope.tripgj.sections) {
-    // ti = time index
-    for (var ti in $scope.tripgj.sections[s].properties.times) {
-      totalTime = ($scope.tripgj.sections[s].properties.times[ti] - start_ts);
-      data.push({x: totalTime, y: $scope.tripgj.sections[s].properties.speeds[ti] });
-    }
-  }
-  var dataset = {
-      values: data,
-      key: $translate.instant('details.speed'),
-      color: '#7777ff',
-    }
-  var chart = nv.models.lineChart()
-                .margin({left: 65, right: 10})  //Adjust chart margins to give the x-axis some breathing room.
-                .useInteractiveGuideline(false)  //We want nice looking tooltips and a guideline!
-                .x(function(t) {return t.x / 60})
-                .showLegend(true)       //Show the legend, allowing users to turn on/off line series.
-                .showYAxis(true)        //Show the y-axis
-                .showXAxis(true);        //Show the x-axis
-  chart.xAxis
-    .tickFormat(d3.format(".1f"))
-    .axisLabel($translate.instant('details.time') + ' (mins)');
-
-  chart.yAxis     //Chart y-axis settings
-      .axisLabel($translate.instant('details.speed') + ' (m/s)')
-      .tickFormat(d3.format('.1f'));
-
-  d3.select('#chart svg')    //Select the <svg> element you want to render the chart in.
-      .datum([dataset,])         //Populate the <svg> element with chart data...
-      .call(chart);          //Finally, render the chart!
-
-
-  //Update the chart when window resizes.
-  nv.utils.windowResize(chart.update);
-  nv.addGraph(chart);
+  // console.log("trip.start_place = " + JSON.stringify($scope.trip.start_place));
+  //
+  // var data  = [];
+  // var start_ts = $scope.trip.properties.start_ts;
+  // var totalTime = 0;
+  // for (var s in $scope.tripgj.sections) {
+  //   // ti = time index
+  //   for (var ti in $scope.tripgj.sections[s].properties.times) {
+  //     totalTime = ($scope.tripgj.sections[s].properties.times[ti] - start_ts);
+  //     data.push({x: totalTime, y: $scope.tripgj.sections[s].properties.speeds[ti] });
+  //   }
+  // }
+  // var dataset = {
+  //     values: data,
+  //     key: $translate.instant('details.speed'),
+  //     color: '#7777ff',
+  //   }
+  // var chart = nv.models.lineChart()
+  //               .margin({left: 65, right: 10})  //Adjust chart margins to give the x-axis some breathing room.
+  //               .useInteractiveGuideline(false)  //We want nice looking tooltips and a guideline!
+  //               .x(function(t) {return t.x / 60})
+  //               .showLegend(true)       //Show the legend, allowing users to turn on/off line series.
+  //               .showYAxis(true)        //Show the y-axis
+  //               .showXAxis(true);        //Show the x-axis
+  // chart.xAxis
+  //   .tickFormat(d3.format(".1f"))
+  //   .axisLabel($translate.instant('details.time') + ' (mins)');
+  //
+  // chart.yAxis     //Chart y-axis settings
+  //     .axisLabel($translate.instant('details.speed') + ' (m/s)')
+  //     .tickFormat(d3.format('.1f'));
+  //
+  // d3.select('#chart svg')    //Select the <svg> element you want to render the chart in.
+  //     .datum([dataset,])         //Populate the <svg> element with chart data...
+  //     .call(chart);          //Finally, render the chart!
+  //
+  //
+  // //Update the chart when window resizes.
+  // nv.utils.windowResize(chart.update);
+  // nv.addGraph(chart);
 
   /* START: ng-walkthrough code */
   // Tour steps
